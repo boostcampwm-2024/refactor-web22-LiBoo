874961576ed1bd3624c451826603209fdc849750
Changhyun-Hong <alex963@naver.com>
2025-01-20 17:12:18 +0900

    refactor: boostbanner 컴포넌트 분리

---
+++ frontend/src/components/common/BoostBanner.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+import { ASSETS } from '@constants/assets';
+
+const BoostBanner = () => {
+  return (
+    <BannerLink href="https://boostcamp.connect.or.kr/" target="_blank" rel="noopener noreferrer">
+    <Banner src={ASSETS.IMAGES.BANNER.CLIENT} alt="Client Banner" />
+  </BannerLink>
+  );
+};
+
+export default BoostBanner;
+
+const BannerLink = styled.a`
+  display: block;
+  margin-top: 24px;
+  border-radius: 7px;
+  overflow: hidden;
+  transition: opacity 0.2s;
+
+  &:hover {
+    opacity: 0.9;
+  }
+`;
+
+const Banner = styled.img`
+  width: 100%;
+  aspect-ratio: 16 / 2;
+  object-fit: cover;
+  display: block;
+`;
